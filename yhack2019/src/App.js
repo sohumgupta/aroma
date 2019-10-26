53beb704605c4fce96cbba8611dede0049d95b47
Sohum Gupta <sohumgupta99@gmail.com>
2019-10-26 00:32:59 -0400

    lmao article page i guess?

--- yhack2019/src/App.js
+++ yhack2019/src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ArticlePage from './article-page/article-page.js';
+import CreateArticlePage from './create-article/create-article.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<CreateArticlePage/>
     </div>
   );
 }
