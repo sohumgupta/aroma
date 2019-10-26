04afed6aa55dba7e2dbc4bd3e1900cf41386667f
ryanhkim <ryanhkim00@gmail.com>
2019-10-26 19:44:56 -0400

    article content works within state

--- yhack2019/src/main-editor/main-editor.js
+++ yhack2019/src/main-editor/main-editor.js
@@ -1,35 +1,55 @@
 import React from 'react';
 import './main-editor.scss';
 import Dante from 'Dante2';
+import EditorHeader from '../components/editor-header';
 
 
 class MainEditor extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {value: ''};
+		this.state = {
+      editorBlocks: [],
+      value: ""
+    };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-	}
+  }
+  
 	
-	handleChange(event) {
-    this.setState({value: event.target.value});
+  async handleSubmit(event) {
+    this.state.value = this.state.editorBlocks.reduce((acc, x) => acc + x.text + "\n", "");
+    console.log(this.state.value);
   }
 
-  handleSubmit(event) {
-    alert('An article was submitted: ' + this.state.value);
+  concatValue = (inText) => {
+    this.setState ({
+      value: this.state.value + "hello" + inText
+    })
+  }
+
+  updateEditor = (newBlocks) => {
+    this.setState ({
+      editorBlocks: newBlocks
+    })
   }
 	
   render() {
 		return(
-			<div className="content">
-        <div className="dante-container">
-          <Dante className="text" onChange={() => console.log("hello")} />
+      <div className="editor-page-container">
+        <EditorHeader />
+        <div className="content">
+          <div className="dante-container">
+            <Dante className="text" onChange={(editor) => 
+            this.updateEditor(editor.emitSerializedOutput().blocks)} 
+            // console.log(editor.emitSerializedOutput().blocks)}
+            />
+          </div>
+          <div className="submit-container">
+            <input onClick={this.handleSubmit} className="submit-button" value="publish" />
+          </div>
         </div>
-        <div className="submit-container">
-          <input className="submit-button" value="submit" />
-        </div>
-			</div>
+        {this.state.value}
+      </div>
 		);
   }
 }
