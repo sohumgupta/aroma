6e5e7c922dab4294320c86aa0a206bddc09c5deb
ryanhkim <ryanhkim00@gmail.com>
2019-10-26 00:32:42 -0400

    finished header for site
    header can be used and adapted for landing and city pages

---
+++ yhack2019/src/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import './Landing.css';
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
